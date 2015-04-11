7319c95597fca6452fbb76cc997aa851a0ca46ab
Xùdōng Wú <wooxudong@gmail.com>
2015-04-12 01:43:49 +0800

    google login

--- js/login.js
+++ js/login.js
@@ -75,3 +75,72 @@ window.fbAsyncInit = function() {
         $('#fb-root').append(e);
     }());
 
+
+//google login functions
+    (function() {
+       var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+       po.src = 'https://apis.google.com/js/client.js?onload=onLoadCallback';
+       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+     })();
+
+     function onLoadCallback()
+    {
+        gapi.client.setApiKey('AIzaSyBjLBRfKdgND__xGZaY6z4SBltIxf1fXq8'); //set your API KEY
+        gapi.client.load('plus', 'v1',function(){});//Load Google + API
+    }
+
+    function google_login() 
+    {
+      var myParams = {
+        'clientid' : '980224079160-bmsd1d9rpiatcfguc987a32appn6j58k.apps.googleusercontent.com', //You need to set client id
+        'cookiepolicy' : 'single_host_origin',
+        'callback' : 'loginCallback', //callback function
+        'approvalprompt':'force',
+        'scope' : 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+      };
+      gapi.auth.signIn(myParams);
+    }
+
+    function loginCallback(result)
+    {
+        if(result['status']['signed_in'])
+        { 
+            var request = gapi.client.plus.people.get(
+            {
+                'userId': 'me'
+            });
+            request.execute(function (resp)
+            {
+                var email = '';
+                if(resp['emails'])
+                {
+                    for(i = 0; i < resp['emails'].length; i++)
+                    {
+                        if(resp['emails'][i]['type'] == 'account')
+                        {
+                            email = resp['emails'][i]['value'];
+                        }
+                    }
+                }
+             
+                var str = "Name:" + resp['displayName'] + "<br>";
+                str += "Image:" + resp['image']['url'] + "<br>";
+                str += "<img src='" + resp['image']['url'] + "' /><br>";
+             
+                str += "URL:" + resp['url'] + "<br>";
+                str += "Email:" + email + "<br>";
+                document.getElementById("profile").innerHTML = str;
+            });
+        }   
+     
+    }
+
+   
+
+    function logout()
+    {
+        gapi.auth.signOut();
+        location.reload();
+    }
+
+
