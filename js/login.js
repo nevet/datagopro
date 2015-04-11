820ffabb0b6a429e82c5139e69439364398010f5
Xùdōng Wú <wooxudong@gmail.com>
2015-04-11 17:15:22 +0800

    test facebook

--- js/login.js
+++ js/login.js
@@ -23,49 +23,6 @@
             bPopup.close({
                transitionClose: 'slideUp'
             });
-        });
-
-
-
-
-    window.fbAsyncInit = function() {
-    FB.init({
-        appId   : '1567423636871440',
-        oauth   : true,
-        status  : true, // check login status
-        cookie  : true, // enable cookies to allow the server to access the session
-        xfbml   : true // parse XFBML
-    }); };
-
-    function fb_login(){
-    FB.login(function(response) {
-
-        if (response.authResponse) {
-            console.log('Welcome!  Fetching your information.... ');
-            //console.log(response); // dump complete info
-            access_token = response.authResponse.accessToken; //get access token
-            user_id = response.authResponse.userID; //get FB UID
-
-            FB.api('/me', function(response) {
-                user_email = response.email; //get user email
-          // you can store this data into your database             
-            });
-
-        } else {
-            //user hit cancel button
-            console.log('User cancelled login or did not fully authorize.');
-
-        }
-        }, {
-        scope: 'publish_stream,email'
-      });
-    }   
-    
-    (function() {
-        var e = document.createElement('script');
-        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-        e.async = true;
-        document.getElementById('fb-root').appendChild(e);
-    }());
+        }); 
 })();
 
