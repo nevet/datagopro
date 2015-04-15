e251d35cba0b8e690f19508773a860c35a531fd7
Xùdōng Wú <wooxudong@gmail.com>
2015-04-15 14:05:16 +0800

    tutorial with other nav bars

--- js/help.js
+++ js/help.js
@@ -11,12 +11,26 @@
 	
 	$(document).ready(function(){
 		$(document).on("click","#help",function(event){
-			$('#tutorialguide').joyride({
-	      autoStart : true,
-	 		  nubPosition: 'top',
-	 		  modal:true,
-	 		  expose: true	
-		    });
+			var activeNav = $(".navbar-nav > li.active");
+			var activeId = $(activeNav.find('a')[0]).attr("id");
+			switch(activeId)
+			{
+				case "newdata":
+												$('#newdataguide').joyride({
+												   autoStart : true,
+												 	 nubPosition: 'top',
+												 	 modal:true,
+												 	 expose: true	});
+												break;
+				case "popularinput":
+												break;
+				case "help":
+												break;
+				case "login":
+												break;
+			}
+
+
 		});
 	});
 
