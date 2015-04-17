ef39f822959dca439ea32b665a245822c9d150ba
Huang Da <nevets.st@gmail.com>
2015-04-17 09:19:10 +0800

    fix script error

--- servermng.sh
+++ servermng.sh
@@ -3,13 +3,13 @@
 set passwd [lindex $argv 0]
 set action [lindex $argv 1]
 
-if ($action == "monitor") {
-	spawn ssh admin@datagopro.com sudo /var/www/html/monitor_remote.sh
-} else
-if ($action == "update") {
-	spawn ssh admin@datagopro.com sudo /var/www/html/update_remote.sh
+if { $action == "monitor"} {
+	set cmd "sudo /var/www/html/monitor_remote.sh"
+} elseif { $action == "update"} {
+	set cmd "sudo /var/www/html/update_remote.sh"
 }
 
+spawn ssh admin@datagopro.com $cmd
 expect "assword:"
 send "$passwd\r"
 interact
