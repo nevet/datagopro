431cf55f7b56ccfe6ef49e759dfbc6a11cc47cb1
nevet <nevets.st@gmail.com>
2015-04-22 15:22:18 +0800

    fix detiny.php bug, now can redirect to datasession.php

--- detiny.php
+++ detiny.php
@@ -5,13 +5,13 @@ include("utils.php");
 $db = db_connect();
 $tiny = $_GET["u"];//shortened chars
 
-$sql = "SELECT * FROM urls WHERE unique_chars=$tiny";
+$sql = "SELECT * FROM urls WHERE unique_chars='$tiny'";
 $res = $db->query($sql);
 
-if (!$res && $res->num_rows > 0) {
+if ($res && $res->num_rows > 0) {
   $row = $res->fetch_assoc();
 
-  header("Location:datagopro.com/datasession.php?retrieve=".$row["input"]);
+  header("Location:datasession.php?retrieve=".$row["input"]);
 } else {
   echo "invalid short url";
 }
